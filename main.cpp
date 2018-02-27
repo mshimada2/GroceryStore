79972bd60ac4883c6aade5881bb3eb61380b910e
mshimada <mshimada2@mail.csuchico.edu>
2018-02-27 11:34:48 -0800

    i hate turn in

--- main.cpp
+++ main.cpp
@@ -5,40 +5,55 @@ using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-do{
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-
-    if(input == 'a' || input == 'A'){
-
-    string item;
-
-    cout<<"What is the item?"<<endl;
-    cin>>item; 
-  
-  if(numItems < 5){
-    
-    list[numItems] = item;
-    numItems++;
-
-    }
-  
-  else{
-   cout<<"You'll need a bigger list!"<<endl;
-
-    }
-
-}
-
-}while(input != 'q' && input != 'Q');
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        do{
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
 
 
-return 0;
+
+                if(input == 'a' || input == 'A'){
+
+                        string item;
+
+                        cout<<"What is the item?"<<endl;
+                        cin>>item; 
+
+
+                        if(numItems < 5){
+
+                                list[numItems] = item;
+                                numItems++; 
+
+
+                        }
+
+                        else{
+                                cout<<"You'll need a bigger list!"<<endl;
+
+                        }
+
+                }
+
+        }while(input != 'q' && input != 'Q');
+
+        cout<<"==ITEMS TO BUY==\n";
+
+        int index;
+
+        for(index = 0; index < 5; index++){
+
+
+                cout<<""<<index + 1<<list[index]<<"\n";
+
+
+        }
+
+        return 0;
 }
